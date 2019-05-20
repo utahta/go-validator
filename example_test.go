8d0f811fdd842a8a9909cbf8f8cf4b7fcd7eaafa
utahta <labs.ninxit@gmail.com>
2019-05-21 02:02:51 +0900

    test: add setFunc example test

--- example_test.go
+++ example_test.go
@@ -1,12 +1,13 @@
 package validator_test
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/utahta/go-validator"
 )
 
-func ExampleValidateStruct_simpleSuccess() {
+func ExampleValidateStruct_simple() {
 	type user struct {
 		Name  string `valid:"required,alphanum"`
 		Age   uint   `valid:"required,len(0|116)"`
@@ -19,29 +20,47 @@ func ExampleValidateStruct_simpleSuccess() {
 		Age:   9,
 		Email: "",
 	})
-
 	fmt.Println(err)
 
-	// Output:
-	// <nil>
-}
-
-func ExampleValidateStruct_simpleFailure() {
-	type user struct {
-		Name  string `valid:"required,alphanum"`
-		Age   uint   `valid:"required,len(0|116)"`
-		Email string `valid:"optional,email"`
-	}
-
-	v := validator.New()
-	err := v.ValidateStruct(&user{
+	err = v.ValidateStruct(&user{
 		Name:  "_",
 		Age:   200,
 		Email: "invalid",
 	})
-
 	fmt.Println(err)
 
 	// Output:
+	// <nil>
 	// Name: '_' does validate as 'alphanum';Age: '200' does validate as 'len(0|116)';Email: 'invalid' does validate as 'email'
+}
+
+func ExampleValidateStruct_setFunc() {
+	type content struct {
+		Type string `valid:"contentType(image/jpeg|image/png|image/gif)"`
+	}
+
+	v := validator.New(
+		validator.WithFunc("contentType", func(_ context.Context, f validator.Field, opt validator.FuncOption) (bool, error) {
+			v := f.Value().String()
+			for _, param := range opt.Params {
+				if v == param {
+					return true, nil
+				}
+			}
+			return false, nil
+		}),
+	)
+	err := v.ValidateStruct(&content{
+		Type: "image/jpeg",
+	})
+	fmt.Println(err)
+
+	err = v.ValidateStruct(&content{
+		Type: "image/bmp",
+	})
+	fmt.Println(err)
+
+	// Output:
+	// <nil>
+	// Type: 'image/bmp' does validate as 'contentType(image/jpeg|image/png|image/gif)'
 }
